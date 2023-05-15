15a98c282476923b31e57b5e195be6fa977225a8
sameemul-haque <110324374+sameemul-haque@users.noreply.github.com>
2023-05-15 20:23:55 +0530

    changed the position of submit button to the right side of subject code input

--- public/script.js
+++ public/script.js
@@ -4,8 +4,8 @@ const submitButton = document.getElementById('submit-button');
 
 university.addEventListener('change', () => {
   if (university.value) {
-    subjectCode.style.display = 'block';
-    submitButton.style.display = 'block';
+    subjectCode.style.display = 'inline-block';
+    submitButton.style.display = 'inline-block';
   } else {
     subjectCode.style.display = 'none';
     submitButton.style.display = 'none';
