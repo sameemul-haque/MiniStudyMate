773a02f511f574b5f838281f8a23009a2659e085
sameemul-haque <110324374+sameemul-haque@users.noreply.github.com>
2023-05-17 21:17:31 +0530

    css added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Header from './components/Header';
 import Form from './components/Form';
 import Footer from './components/Footer';
+import './App.css'; 
 
 function App() {
   return (
     <div>
       <Header />
+      <br/><br/><br/><br/>
       <Form />
       <Footer />
     </div>
