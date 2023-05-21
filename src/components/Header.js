5726a0215f3792250b978ef52d956020d11b93c4
sameemul-haque <110324374+sameemul-haque@users.noreply.github.com>
2023-05-21 17:58:28 +0530

    updated the header and css for better view - in mobile devices as well

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,23 @@
 import { Icon } from "@iconify/react";
+
 function Header() {
   return (
     <header>
+      <div className="logout-wrapper">
+        <nav>
+          <a href="login.html">
+            <Icon style={{ fontSize: 20 }} icon="material-symbols:logout" />{" "}
+            LOGOUT
+          </a>
+        </nav>
+      </div>
       <h1>
         <Icon style={{ fontSize: 35 }} icon="game-icons:graduate-cap" />
         StudyMate
       </h1>
       <p id="head-caption">A Web-based Study Material Retrieval System</p>
-      <nav>
-        <a href="login.html">
-          <Icon style={{ fontSize: 20 }} icon="material-symbols:logout" />{" "}
-          LOGOUT
-        </a>
-      </nav>
     </header>
   );
 }
+
 export default Header;
