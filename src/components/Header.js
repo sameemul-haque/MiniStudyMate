72a75a6441f37b8e38aeeb14b5143ff6d9c0e387
sameemul-haque <110324374+sameemul-haque@users.noreply.github.com>
2023-05-27 06:31:17 +0530

    added sign out button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,11 @@
 import { Icon } from "@iconify/react";
+import { auth } from "../firebase-config";
 
 function Header() {
   return (
     <header>
       <div className="logout-wrapper">
-        {/* <nav>
-          <a href="login.html">
-            <Icon style={{ fontSize: 20 }} icon="material-symbols:logout" />{" "}
-            LOGOUT
-          </a>
-        </nav> */}
+        <SignOut />
       </div>
       <h1>
         <Icon style={{ fontSize: 35 }} icon="game-icons:graduate-cap" />
@@ -17,6 +13,16 @@ function Header() {
       </h1>
       <p id="head-caption">A Web-based Study Material Retrieval System</p>
     </header>
+  );
+}
+
+function SignOut() {
+  return (
+    auth.currentUser && (
+      <button className="sign-out" onClick={() => auth.signOut()}>
+        Sign Out
+      </button>
+    )
   );
 }
 
