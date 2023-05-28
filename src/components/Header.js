381993281cc884cd9b21422dfe42ded6e275f38b
sameemul-haque <110324374+sameemul-haque@users.noreply.github.com>
2023-05-28 14:22:53 +0530

    added user icon in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Icon } from "@iconify/react";
 import { auth } from "../firebase-config";
 
@@ -17,18 +18,35 @@ function Header() {
 }
 
 function SignOut() {
+  const [showDropdown, setShowDropdown] = useState(false);
   const userEmail = auth.currentUser?.email;
+
+  const handleDropdownToggle = () => {
+    setShowDropdown(!showDropdown);
+  };
+
+  const handleSignOut = () => {
+    auth.signOut();
+  };
+
   return (
     auth.currentUser && (
-      <div className="user-info">
-        <span>
-          <Icon style={{ fontSize: 10 }} icon="logos:google-gmail" />{" "}
-          {userEmail}
-        </span>
-        <button className="sign-out" onClick={() => auth.signOut()}>
-          <Icon style={{ fontSize: 20 }} icon="material-symbols:logout" />
-          {" SIGN OUT"}
-        </button>
+      <div>
+        <div className="user-icon" onClick={handleDropdownToggle}>
+          <Icon style={{ fontSize: 30 }} icon="line-md:account" />
+        </div>
+        {showDropdown && (
+          <div className="user-info">
+            <span>
+              <Icon style={{ fontSize: 10 }} icon="logos:google-gmail" />{" "}
+              {userEmail}
+            </span>
+            <button className="sign-out" onClick={handleSignOut}>
+              <Icon style={{ fontSize: 20 }} icon="material-symbols:logout" />
+              {" SIGN OUT"}
+            </button>
+          </div>
+        )}
       </div>
     )
   );
